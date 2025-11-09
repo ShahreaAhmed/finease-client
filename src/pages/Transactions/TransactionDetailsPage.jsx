6f73992135ad90a6ee89ac01f1ed118b9869daf0
Shahrea Ahmed <shahrearuman@gmail.com>
2025-11-10 01:05:24 +0600

    first commit

---
+++ src/pages/Transactions/TransactionDetailsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransactionDetailsPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default TransactionDetailsPage;
