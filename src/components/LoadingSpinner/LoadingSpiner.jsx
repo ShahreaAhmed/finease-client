b8cab90622dc7f4b67aa2f686abc88ed8d028141
Shahrea Ahmed <shahrearuman@gmail.com>
2025-11-13 23:36:41 +0600

    assignment complete

--- src/components/LoadingSpinner/LoadingSpiner.jsx
+++ src/components/LoadingSpinner/LoadingSpiner.jsx
@@ -4,14 +4,12 @@ const LoadingSpinner = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-br from-indigo-50 via-purple-50 to-pink-50 z-50">
       <div className="relative flex items-center justify-center">
-        {/* Outer glowing ring */}
+ 
         <div className="w-24 h-24 rounded-full border-4 border-transparent border-t-indigo-500 border-r-pink-500 animate-spin shadow-[0_0_20px_rgba(99,102,241,0.5)]"></div>
 
-        {/* Inner soft pulse circle */}
         <div className="absolute w-10 h-10 rounded-full bg-gradient-to-r from-indigo-500 to-pink-500 animate-pulse blur-sm"></div>
       </div>
 
-      {/* Optional loading text */}
       <span className="absolute bottom-24 text-gray-700 text-lg font-semibold tracking-wide animate-pulse">
         Loading...
       </span>
--- src/pages/Transactions/MyTransactionPage.jsx
+++ src/pages/Transactions/MyTransactionPage.jsx
@@ -1,51 +1,10 @@
-// import React, { useState } from "react";
-// import { useLoaderData } from "react-router";
-// import TransactionHistoryCard from "../../components/TransactionHistoryCard/TransactionHistoryCard";
-
-// const MyTransactionPage = () => {
-
-//   const loaderData = useLoaderData()
-
-//   const [transactions, setTransactions] = useState(loaderData)
-
-//   const handleDeleteTransaction = (id) => {
-//     setTransactions((transaction) => transaction.filter((card) => card._id !== id))
-//   }
-  
-//   console.log(loaderData)
-
-//   return (
-//     <div>
-//       <div className="text-center text-2xl font-bold">Transaction History</div>
-//       <div>
-//         {transactions.length > 0 ? (
-//           transactions.map((transaction) => (<TransactionHistoryCard transaction={transaction} key={transaction._id}
-//             onDelete={handleDeleteTransaction}
-//           ></TransactionHistoryCard>))) : (
-//             <p className="text-center text-gray-400 mt-10 text-lg">No transaction found.</p>
-          
-//         )}
-        
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default MyTransactionPage;
-
-
-
-
-
-
-
 
 import React, { useState, useEffect, useContext } from "react";
 import TransactionHistoryCard from "../../components/TransactionHistoryCard/TransactionHistoryCard";
 import { AuthContext } from "../../context/AuthContext";
 
 const MyTransactionPage = () => {
-  const { user } = useContext(AuthContext); // get logged-in user
+  const { user } = useContext(AuthContext); 
   const [transactions, setTransactions] = useState([]);
   const [sortField, setSortField] = useState("date");
   const [sortOrder, setSortOrder] = useState("desc");
